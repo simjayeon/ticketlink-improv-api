3bd6451dcef0787dcdc4f807cde63ce31f843608
nature <simjayeon98@gmail.com>
2025-07-04 17:40:58 +0900

    [DOCS] 초기 셋팅

---
+++ src/main/java/com/kbo/ticketlinkimprovapi/TicketlinkImprovApiApplication.java
@@ -0,0 +1,13 @@
+package com.kbo.ticketlinkimprovapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketlinkImprovApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketlinkImprovApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kbo/ticketlinkimprovapi/TicketlinkImprovApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbo.ticketlinkimprovapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketlinkImprovApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
